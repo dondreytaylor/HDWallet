bb76ff454bf7116721e984630605d16b73cbc46b
Dondrey Taylor <dondrey.taylor@gmail.com>
2019-12-29 16:47:37 -0500

    Added Bithereum

--- HDWalletKit/Wallet/PublicKey.swift
+++ HDWalletKit/Wallet/PublicKey.swift
@@ -34,6 +34,7 @@ public struct PublicKey {
         case .bitcoin: fallthrough
         case .dash: fallthrough
         case .bitcoinCash: fallthrough
+        case .bithereum: fallthrough
         case .litecoin:
             return generateBtcAddress()
         case .ethereum:
@@ -43,7 +44,7 @@ public struct PublicKey {
     
     public var utxoAddress: Address {
         switch coin {
-        case .bitcoin, .litecoin, .dash, .bitcoinCash:
+        case .bitcoin, .litecoin, .dash, .bitcoinCash, .bithereum:
             return try! LegacyAddress(address, coin: coin)
         case .ethereum:
             fatalError("Coin does not support UTXO address")
